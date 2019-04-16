b35347c083662ef6c57bcba2937849c69bf4c8b2
Donghee Han <hdh7485@gmail.com>
2019-04-16 22:40:15 +0900

    Add prius_to_lcm_node

---
+++ src/prius_to_lcm_node.cpp
@@ -0,0 +1,41 @@
+#include <ros/ros.h>
+//#include <prius_msgs/
+#include <nav_msgs/Odometry.h>
+#include <cmath>
+
+class PriusToLCM {
+private:
+  ros::Subscriber sub_;
+  ros::NodeHandle nh_;
+  ros::NodeHandle nh;
+
+  double x_;
+  double y_;
+  double yaw_;
+  double velocity_;
+  double yaw_rate_;
+public:
+  PriusToLCM():nh_("~") {
+    sub_ = nh.subscribe<nav_msgs::Odometry>("/base_pose_ground_truth", 100, &PriusToLCM::navMsgCallback, this);
+  }
+
+  void navMsgCallback(const nav_msgs::Odometry::ConstPtr& msg) {
+    x_ = msg->pose.pose.position.x;
+    y_ = msg->pose.pose.position.y;
+    yaw_ = msg->twist.twist.angular.z;
+    velocity_ = std::sqrt(std::pow(msg->twist.twist.linear.x, 2) + std::pow(msg->twist.twist.linear.y, 2));
+    yaw_rate_ = msg->twist.twist.angular.z;
+    ROS_INFO_STREAM("x:" << x_ << " y:" << y_ << " yaw:" << yaw_ << " vel:" << velocity_ << " yaw_rate:" << yaw_rate_);
+  }
+  
+};
+
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "prius_to_lcm_node");
+  PriusToLCM p2l;
+
+  ros::spin();
+
+  return 0;
+}
